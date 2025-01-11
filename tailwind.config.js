3b68005cf366b00cf29b618dd04629339f8e7107
razaqultegar <myrazaqul@gmail.com>
2025-01-11 19:28:44 +0700

    build: configure tailwindcss settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ module.exports = {
         extend: {
             colors: {
                 white: "#ffffff",
+                mercury: "#e8e8e8",
+                coal: "#f2f3f4",
+                skull: "#f8f8f8",
                 mineshaft: "#3a3a3a",
-                cerulean50: "#40a8e6",
+                onyx: "#6a6a6a",
+                cerulean: {
+                    10: "#e6f5fb",
+                    50: "#40a8e6",
+                },
+                clrPrimary: "#00aeef",
+                clrSubText: "#989898",
+                clrField05: "#f7f7f7",
             },
             fontFamily: {
                 sans: [
@@ -29,10 +39,22 @@ module.exports = {
                 ],
             },
             fontSize: {
+                xs: "0.625rem",
+                sm: "0.75rem",
                 body: "0.875rem",
             },
             maxWidth: {
                 "screen-sm": "480px",
+                480: "480px",
+            },
+            width: {
+                480: "480px",
+            },
+            height: {
+                header: "60px",
+            },
+            zIndex: {
+                1: "1",
             },
         },
     },
