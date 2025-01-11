e5fbf71da7fc9c9803b54e446234c4f38dcb5de1
razaqultegar <myrazaqul@gmail.com>
2025-01-11 09:58:56 +0700

    build: configure tailwindcss settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
     content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                white: "#ffffff",
+                mineshaft: "#3a3a3a",
+                cerulean50: "#40a8e6",
+            },
+            fontFamily: {
+                sans: [
+                    "Rubik",
+                    "Rubik Fallback",
+                    "ui-sans-serif",
+                    "system-ui",
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    "Segoe UI",
+                    "Roboto",
+                    "Helvetica Neue",
+                    "Arial",
+                    "Noto Sans",
+                    "sans-serif",
+                    "Apple Color Emoji",
+                    "Segoe UI Emoji",
+                    "Segoe UI Symbol",
+                    "Noto Color Emoji",
+                ],
+            },
+            fontSize: {
+                body: "0.875rem",
+            },
+            maxWidth: {
+                "screen-sm": "480px",
+            },
+        },
     },
     plugins: [],
 };
