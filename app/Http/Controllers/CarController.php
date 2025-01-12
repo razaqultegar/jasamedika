15cd25b01a641de8232e30ca7e2d5f6663bec61e
razaqultegar <myrazaqul@gmail.com>
2025-01-12 22:26:26 +0700

    feat: add filter car by conditions

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -11,17 +11,26 @@ class CarController extends Controller
     public function index(Request $request)
     {
         $data['title'] = 'Mobil';
+        $data['model'] = Car::select('model',)->groupBy('model')->get();
         $query = Car::query();
 
         if ($request->has('date_range') && !empty($request->date_range)) {
+            session()->put('date_range', $request->date_range);
             $dates = explode(' - ', $request->date_range);
 
             if (count($dates) === 2) {
                 $startDate = $dates[0];
                 $endDate = $dates[1];
 
-                // Filter cars based on availability
-                $query->whereDoesntHave('orders', function ($q) use ($startDate, $endDate) {
+                if ($request->has('merk') && !empty($request->merk)) {
+                    $query->where('merk', $request->merk);
+                }
+
+                if ($request->has('model') && !empty($request->model)) {
+                    $query->where('model', $request->model);
+                }
+
+                $query->with(['orders' => function ($q) use ($startDate, $endDate) {
                     $q->where(function ($subQuery) use ($startDate, $endDate) {
                         $subQuery->whereBetween('start_date', [$startDate, $endDate])
                             ->orWhereBetween('end_date', [$startDate, $endDate])
@@ -30,7 +39,11 @@ class CarController extends Controller
                                     ->where('end_date', '>=', $endDate);
                             });
                     });
-                });
+                }]);
+
+                if ($request->has('available') && !empty($request->available)) {
+                    $query->orderByRaw('(SELECT COUNT(*) FROM orders WHERE orders.car_id = cars.id AND ((orders.start_date BETWEEN ? AND ?) OR (orders.end_date BETWEEN ? AND ?) OR (orders.start_date <= ? AND orders.end_date >= ?))) ASC', [$startDate, $endDate, $startDate, $endDate, $startDate, $endDate]);
+                }
 
                 $start = Carbon::createFromFormat('Y-m-d', $startDate);
                 $end = Carbon::createFromFormat('Y-m-d', $endDate);
@@ -42,7 +55,12 @@ class CarController extends Controller
             }
         }
 
-        $data['cars'] = $query->get();
+        $cars = $query->get()->map(function ($car) {
+            $car->is_booked = $car->orders->isNotEmpty();
+            return $car;
+        });
+
+        $data['cars'] = $cars;
 
         return view('cars.index', $data);
     }
